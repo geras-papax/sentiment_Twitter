7846bfbafcd402bba07aebb6ff89ab473584da53
Gerasimos Papachronopoulos <58421904+geras-papax@users.noreply.github.com>
2022-02-23 13:32:38 +0200

    Update

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from logisticRegression import logistic_regression
 from lstm import lstm, preprocess, prediction
 
 X_train, X_test, y_train, y_test, raw_X_train, raw_X_test = getData()
-# naive_bayes(X_train, X_test, y_train, y_test)
-# logistic_regression(X_train, X_test, y_train, y_test)
+naive_bayes(X_train, X_test, y_train, y_test)
+logistic_regression(X_train, X_test, y_train, y_test)
 tokenizer,model = lstm(raw_X_train, raw_X_test, y_train,  y_test)
 print("Insert your Tweet:")
 print("------------------")
